8d133c42ad2d56b2b4068efb30f1437213149b26
Aniket Chauhan <chauhananiket12801@gmail.com>
2023-10-16 17:13:54 +0530

    basic counter app using provider package.

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:developer';
+
+import 'package:counter_provider/provider/counter_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ChangeNotifierProvider(
+        create: (context) => CounterProvider(),
+        child: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final CounterProvider counter = Provider.of(context, listen: false);
+
+    log("Scaffold build"); // Only build first time
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Consumer<CounterProvider>(
+              builder: (BuildContext context, CounterProvider counter,
+                  Widget? child) {
+                log("Only Text build"); // build whenever count changes
+
+                return Text(
+                  '${counter.count}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counter.incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
